591cc61d3033c466d31fd4bc36768715226628cb
Sun Xiaoqi <sunxq1991@gmail.com>
2016-03-24 09:59:53 +0800

    modified:   src/com/algorithms/string/LongestCommonSubstring.java

--- src/com/algorithms/string/LongestCommonSubstring.java
+++ src/com/algorithms/string/LongestCommonSubstring.java
@@ -4,6 +4,9 @@ package com.algorithms.string;
  * Created by sunny on 16/3/23.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 最长公共子串（公共子串是连续substring，不同于公共子序列）
  * 指求给定的一组字符串长度最大的共有的子串的问题。
@@ -19,12 +22,12 @@ public class LongestCommonSubstring {
      * http://blog.csdn.net/liufeng_king/article/details/8528858
      * 设opt(i,j)表示前缀子串对x[1: i] ,y[1:j]的最长公共后缀
      * opt(i,j) = opt(i-1,j-1) + x(i)   if x(i) = y(j)
-     *          = ""                    ifx(i) != y(j)
+     *          = ""                    if x(i) != y(j)
      */
-    public static String lcsDp(String x, String y) {
+    public static List<String> lcsDp(String x, String y) {
         int m = x.length(), n = y.length();
         int i, j;
-        String ret = "";
+        List<String> ret = new ArrayList<String>();
 
         //初始化opt
         String[][] opt = new String[m + 1][n + 1];
@@ -47,9 +50,10 @@ public class LongestCommonSubstring {
 
                 if (opt[i][j].length() > lcsLen) {
                     lcsLen = opt[i][j].length();
-                    ret = opt[i][j];
+                    ret.clear();
+                    ret.add(opt[i][j]);
                 } else if (opt[i][j].length() == lcsLen) {
-                    ret += "\n" + opt[i][j];
+                    ret.add(opt[i][j]);
                 }
             }
         }
@@ -58,9 +62,38 @@ public class LongestCommonSubstring {
 
     /**
      * 利用后缀数组实现
+     * 将两个字符串s1和s2进行拼接s=s1#s2
+     * 根据后缀的最长公共前缀height[]进行求解
+     * 最长公共子串(poj2774)  只要求求长度
      */
+    public static int lcsSa(String s1, String s2) {
+        String s = s1 + "#" + s2;
+        int m = s1.length(), n = s2.length();
+        //计算后缀数组
+        int[] sa = SuffixArray.buildSuffixArray(s);
+        int[] rank = SuffixArray.buildRank(s, sa);
+        int[] height = SuffixArray.buildHeight(s, sa, rank);
+
+        //寻找最长公共子串
+        List<String> ret = new ArrayList<String>();
+        int lcsLen = 0;
+        for (int i = 1; i < height.length; i++) {
+            if (height[i] > lcsLen) {
+                //保证两个后缀子串分别位于s1和s2中
+                if (sa[i - 1] < m && sa[i] > m || sa[i - 1] > m && sa[i] < m) {
+                    lcsLen = height[i];
+                }
+            } else if (height[i] == lcsLen) {
+                ;
+            }
+        }
+        return lcsLen;
+    }
+
     public static void main(String[] args) {
-        System.out.println(lcsDp("abab", "baba"));
+        String s1 = "abbad", s2 = "baba";
+        System.out.println(lcsDp(s1, s2));
+        System.out.println(lcsSa(s1, s2));
     }
 
 }
--- src/com/algorithms/string/SuffixArray.java
+++ src/com/algorithms/string/SuffixArray.java
@@ -16,9 +16,11 @@ import java.util.Comparator;
 
 public class SuffixArray {
 
-    //最直接的方法构建后缀数组
-    //Time: O(n^2lgn) 排序复杂度O(nlgn),compare复杂度O(n)
-    // SA[i]表示排名第i的后缀在str中的起始位置
+    /**
+     * 最直接的方法构建后缀数组
+     * Time: O(n^2lgn) 排序复杂度O(nlgn),compare复杂度O(n)
+     * SA[i]表示排名第i的后缀在str中的起始位
+     */
     public static int[] buildSuffixArray(final String str) {
         Integer[] sa = new Integer[str.length()];
         for (int i = 0; i < str.length(); i++) {
@@ -38,8 +40,7 @@ public class SuffixArray {
         return ret;
     }
 
-    public static int[] buildRank(String str) {
-        int[] sa = buildSuffixArray(str);
+    public static int[] buildRank(String str, int[] sa) {
         int[] rank = new int[sa.length];
         for (int i = 0; i < sa.length; i++) {
             rank[sa[i]] = i;
@@ -49,8 +50,7 @@ public class SuffixArray {
 
     // Height[i]表示后缀SA[i]和SA[i-1]的最长公共前缀(Longest Common Prefix,LCP)
     // 简记为Height[i]=LCP(SA[i],SA[i-1])
-    public static int[] buildHeight(String str) {
-        int[] sa = buildSuffixArray(str);
+    public static int[] buildHeight(String str, int[] sa) {
         int[] height = new int[sa.length];
         height[0] = 0;
 
@@ -66,14 +66,9 @@ public class SuffixArray {
         return height;
     }
 
-    public static int[] buildHeight2(String str) {
+    //通过rank[] 更高效
+    public static int[] buildHeight(String str, int[] sa, int[] rank) {
         int len = str.length();
-        int[] sa = buildSuffixArray(str);
-        int[] rank = new int[len];
-        for (int i = 0; i < len; i++) {
-            rank[sa[i]] = i;
-        }
-
         int i, k, h = 0;
         int[] height = new int[len];
         for (i = 0; i < str.length(); i++) {
@@ -104,14 +99,22 @@ public class SuffixArray {
         for (int i : sa) {
             System.out.print(i + " ");
         }
+
+        System.out.print("\nrank: ");
+        int[] rank = buildRank(str, sa);
+        for (int i : rank) {
+            System.out.print(i + " ");
+        }
+
         System.out.print("\nheight1: ");
-        int[] h = buildHeight(str);
+        int[] h = buildHeight(str, sa);
         for (int i : h) {
             System.out.print(i + " ");
         }
+
         System.out.print("\nheight2: ");
-        h = buildHeight2(str);
-        for (int i : h) {
+        int[] h2 = buildHeight(str, sa, rank);
+        for (int i : h2) {
             System.out.print(i + " ");
         }
     }
