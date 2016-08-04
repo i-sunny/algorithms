89715d537a6036ac0d857c9d29efb8f6c851af25
Sun Xiaoqi <sunxq1991@gmail.com>
2016-08-04 16:08:10 +0800

    图的深度拷贝

---
+++ src/com/algorithms/graph/CloneGraph.java
@@ -0,0 +1,56 @@
+package com.algorithms.graph;
+
+import java.util.*;
+
+/**
+ * 递归
+ * clonedMap用来维护已经clone的node,注意clonedMap需要指向新的node(即克隆后的node)
+ * @author sunny
+ * @version $Id: CloneGraph.java, v 0.1 2016-08-04 15:30 xiaoqi.sxq Exp $
+ */
+public class CloneGraph {
+
+    private static Map<Integer, UndirectedGraphNode> clonedMap = new HashMap<Integer, UndirectedGraphNode>();
+
+    public static UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+        if (clonedMap.containsKey(node.value)) {
+            return clonedMap.get(node.value);
+        }
+
+        UndirectedGraphNode cloneNode = new UndirectedGraphNode(node.value);
+        clonedMap.put(node.value, cloneNode);
+        for (UndirectedGraphNode e : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(e));
+        }
+        return cloneNode;
+    }
+
+    public static void main(String[] args) {
+        UndirectedGraphNode node1 = new UndirectedGraphNode(1);
+        UndirectedGraphNode node2 = new UndirectedGraphNode(2);
+        UndirectedGraphNode node3 = new UndirectedGraphNode(3);
+        UndirectedGraphNode node4 = new UndirectedGraphNode(4);
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node3);
+        node1.neighbors.add(node4);
+        node3.neighbors.add(node1);
+        node3.neighbors.add(node4);
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+
+        UndirectedGraphNode cloned = cloneGraph(node1);
+        return;
+    }
+}
+
+class UndirectedGraphNode {
+    int value;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int value) {
+        this.value = value;
+        neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+}
